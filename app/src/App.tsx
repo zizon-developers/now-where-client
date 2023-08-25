8b1b8afbd744158a8283b87b11c721f2555e44fb
Hoseong On <ohs8283@naver.com>
2023-08-25 18:05:20 +0900

    feat: 라우팅 설정

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,19 +1,21 @@
-import { useState } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import LoginPage from './pages/Loginpage';
+import KakaoOAuthRedirectPage from './pages/KakaoOAuthRedirectPage';
 
 const App = () => {
-  const [count, setCount] = useState(0);
-
   return (
-    <main className="w-screen h-screen flex justify-center items-center">
-      <p>count: {count}</p>
-      <button
-        type="button"
-        className="p-4 bg-gray-300 text-white text-lg text-bold"
-        onClick={() => setCount((prev) => prev + 1)}
-      >
-        click!
-      </button>
-    </main>
+    <>
+      <Routes>
+        <Route
+          path="/"
+          element={<LoginPage />}
+        />
+        <Route
+          path="/auth/callback/kakao"
+          element={<KakaoOAuthRedirectPage />}
+        />
+      </Routes>
+    </>
   );
 };
 
