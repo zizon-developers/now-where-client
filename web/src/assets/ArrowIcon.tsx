4e78d65d4d6e92ba59a51e5f5ac3b2c6e9c298d5
Hoseong On <ohs8283@naver.com>
2023-09-09 17:24:11 +0900

    chore : arrowicon 컴포넌트화로 사용

---
+++ web/src/assets/ArrowIcon.tsx
@@ -0,0 +1,33 @@
+const LeftArrowIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="30"
+    height="30"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+  >
+    <path d="M15 19l-7-7 7-7" />
+  </svg>
+);
+
+const RightArrowIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="30"
+    height="30"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+  >
+    <path d="M9 5l7 7-7 7" />
+  </svg>
+);
+
+export { LeftArrowIcon, RightArrowIcon };
