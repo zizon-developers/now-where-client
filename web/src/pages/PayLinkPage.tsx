bc2281433e17433d1b85e760aa9a65166ae45ea7
Hoseong On <ohs8283@naver.com>
2023-09-12 04:19:15 +0900

    refactor : svg 리액트 컴포넌트 수정

--- web/src/pages/PayLinkPage.tsx
+++ web/src/pages/PayLinkPage.tsx
@@ -1,15 +1,18 @@
-import { LeftArrowIcon } from '../assets/ArrowIcon';
-import PayLinkForm from 'hooks/PayLinkFrom';
-import Carousel from 'hooks/Carousel';
+import PayLinkForm from 'components/PayLinkFrom';
+import PayLinkGuideCarousel from 'components/PayLinkGuideCarousel';
+import { ReactComponent as LeftArrowIcon } from '../assets/arrow-icon/arrow_left_icon.svg';
 
 const PayLinkPage = () => {
   return (
     <div className="p-4 lg:p-8">
       <div className="flex items-center mb-14">
-        <LeftArrowIcon />
+        <LeftArrowIcon
+          width="30"
+          height="30"
+        />
         <h1 className="text-2xl font-semibold flex-grow ml-20">송금 링크 등록</h1>
       </div>
-      <Carousel />
+      <PayLinkGuideCarousel />
       <div className="flex justify-between items-center mb-4 mt-12">
         <h2 className="text-xl font-semibold">송금 링크</h2>
       </div>
