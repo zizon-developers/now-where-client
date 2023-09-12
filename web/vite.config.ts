6e1d2f3349fff2a4231ad6fc826003cc50898bad
Hoseong On <ohs8283@naver.com>
2023-09-12 16:03:51 +0900

    chore : svgr 플러그인 설정 수정

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -5,15 +5,7 @@ import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tsconfigPaths(),
-    svgr({
-      svgrOptions: {
-        // svgr options
-      },
-    }),
-  ],
+  plugins: [react(), tsconfigPaths(), svgr()],
   server: {
     port: 3000,
   },
